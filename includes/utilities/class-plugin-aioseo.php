b3a9d1409bbf9c5f50b8ea6eff1adee25bec7942
jruns <jason.runs@proton.me>
2026-04-05 16:33:43 -0400

    Disable more AIOSEO AI features

--- includes/utilities/class-plugin-aioseo.php
+++ includes/utilities/class-plugin-aioseo.php
@@ -35,6 +35,21 @@ class DISAI_Plugin_Aioseo {
 		$this->load_editor_styles();
 	}
 
+	public function remove_ai_block_options() {
+		wp_add_inline_script(
+			'aioseo/js/src/vue/standalone/post-settings/main.js',
+			"document.addEventListener( 'DOMContentLoaded', function() {
+				( function removeFilter() {
+					if ( wp.hooks.hasFilter( 'editor.BlockEdit', 'aioseo/paragraph-placeholder' ) ) {
+						wp.hooks.removeFilter( 'editor.BlockEdit', 'aioseo/paragraph-placeholder' );
+					} else {
+						requestAnimationFrame( removeFilter );
+					}
+				} )();
+			} );"
+		);
+	}
+
 	public function load_editor_styles() {
 		wp_enqueue_style( 'disai-aioseo-editor', plugin_dir_url( __DIR__ ) . 'css/aioseo_editor.css', array(), constant( 'DISAI_VERSION' ) );
 	}
@@ -65,7 +80,14 @@ class DISAI_Plugin_Aioseo {
 	public function run() {
 		add_action( 'add_meta_boxes', array( $this, 'remove_writing_assistant_meta_box' ), 100, 2 );
 		add_action( 'admin_print_styles', array( $this, 'hide_ai_editor_elements' ) );
+		add_filter( 'aioseo_ai_assistant_extend_block_editor_inserter_button', '__return_false' );
+		add_action( 'admin_print_scripts', array( $this, 'remove_ai_block_options' ) );
 		add_action( 'elementor/editor/after_enqueue_scripts', array( $this, 'load_editor_styles' ) );
+
+		// Disable AI image generator buttons
+		add_filter('aioseo_ai_image_generator_extend_image_block_toolbar', '__return_false');
+		add_filter('aioseo_ai_image_generator_extend_image_block_placeholder', '__return_false');
+		add_filter('aioseo_ai_image_generator_extend_featured_image_button', '__return_false');
 
 		// Remove admin menu items
 		add_action( 'admin_bar_menu', array( $this, 'remove_admin_bar_menu_items' ), 99999 );
